bcabd5cb6e27f77a31098e126753c7d53d175308
Darel <darelbavcar1@gmail.com>
2022-09-25 18:26:09 +0200

    Disney clone- React js & styled components

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/profile.jpg" />
+    </Nav>
+  );
+};
+
+export default Header;
+
+const Nav = styled.div`
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  // expands it how far it can
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 100px;
+  height: 60px;
+  cursor: pointer;
+`;
